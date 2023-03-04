bb59cdd4587ff5d5bdb8b6d3ab1113e833f3e8ca
Krystyna Sharhorodska <rinjara@gmail.com>
2023-03-04 18:59:45 +0200

    localStorage fix

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -14,13 +14,32 @@ import logo from '../../images/logo.png';
 import avatar from '../../images/Hansel.png';
 import { load, save } from '../../services/storage';
 
+// load('stats').following ||
+//   load('stats').followers ||
+
 const Card = () => {
-  const [following, setFollowing] = useState(load('stats').following || false);
-  const [followers, setFollowers] = useState(load('stats').followers || 100500);
+  const [following, setFollowing] = useState(loadFollowing());
+  const [followers, setFollowers] = useState(loadFollowers());
 
   useEffect(() => {
     save('stats', { following, followers });
   }, [followers, following]);
+
+  function loadFollowing() {
+    const storedStats = load('stats');
+    if (!storedStats) {
+      return false;
+    }
+    return storedStats.following;
+  }
+
+  function loadFollowers() {
+    const storedStats = load('stats');
+    if (!storedStats) {
+      return 100500;
+    }
+    return storedStats.followers;
+  }
 
   const handleFollow = () => {
     if (following) {
@@ -48,7 +67,11 @@ const Card = () => {
           <li>{777} tweets</li>
           <li>{followers.toLocaleString()} followers</li>
         </List>
-        <StyledButton type="button" onClick={handleFollow} following={following}>
+        <StyledButton
+          type="button"
+          onClick={handleFollow}
+          following={following}
+        >
           {following ? 'Following' : 'Follow'}
         </StyledButton>
       </UserInfo>
